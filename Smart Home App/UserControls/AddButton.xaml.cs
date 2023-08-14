6d0bed09714dbc93ba7a89760a35be951a746ead
justinpalaka <justin.palaka@yahoo.com>
2023-08-13 21:33:59 -0600

    final app

---
+++ Smart Home App/UserControls/AddButton.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Smart_Home_App.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AddButton.xaml
+    /// </summary>
+    public partial class AddButton : UserControl
+    {
+        public AddButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(AddButton));
+
+    }
+}
